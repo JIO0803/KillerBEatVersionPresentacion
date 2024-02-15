d6de76ed78cf623867c042d51652171a2ecfd9e5
JIO0803 <joanigle2005@gmail.com>
2024-02-15 14:30:55 +0100

    15/02/2024

--- Assets/Scripts/CambiarPosCamera.cs
+++ Assets/Scripts/CambiarPosCamera.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 public class CambiarPosCamera : MonoBehaviour
 {
     public GameObject Cmara;
-    public GameObject Sala;
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             Cmara.transform.position = gameObject.transform.position;
-            Sala.SetActive(true);
         }
     }  
     public void OnTriggerStay2D(Collider2D collision)
@@ -18,7 +16,6 @@ public class CambiarPosCamera : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             Cmara.transform.position = gameObject.transform.position;
-            Sala.SetActive(true);
         }
     }
 }
